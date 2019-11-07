af9615e1aca829fe9deb02160f4b619a81a5fb94
Samuel Joseph Pollard <40839788+sjpollard@users.noreply.github.com>
2019-11-07 00:55:04 +0000

    Attempt at task14

---
+++ solutions/haskell/task14.hs
@@ -0,0 +1,12 @@
+collatz :: Int -> Int -> Int
+collatz 1 x = x
+collatz n x 
+    | even n = collatz (div n 2) (x+1)
+    | odd n = collatz (3*n + 1) (x+1)
+
+main :: IO () -> Maybe a
+main = do
+        let a = map (\x->collatz x 1) [1..999999]
+        let b = maximum a
+        let c = findIndex (==b) a
+        return c
